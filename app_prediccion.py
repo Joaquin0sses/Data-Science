1a39d503f5122f8304de672c46f7b5ab9cff5db6
Joaquin0sses <166663787+Joaquin0sses@users.noreply.github.com>
2025-07-02 17:10:32 -0400

    Add files via upload

--- app_prediccion.py
+++ app_prediccion.py
@@ -13,13 +13,34 @@ with open('columnas_modelo.txt', 'r') as f:
 
 st.title("Predicción de Abandono Escolar")
 
-# Entradas del usuario
-COD_DEPE2 = st.selectbox("Dependencia", ['1', '2', '3', '4'])
-RURAL_RBD = st.selectbox("Ruralidad", ['0', '1'])
-ESTADO_ESTAB = st.selectbox("Estado Establecimiento", ['1', '2', '3', '4'])
-COD_GRADO = st.selectbox("Grado", ['1', '2', '3', '4'])
-COD_JOR = st.selectbox("Jornada", ['1', '2', '3'])
-GEN_ALU = st.selectbox("Género", ['1', '2'])
+# Diccionarios para mostrar opciones descriptivas
+depe_dict = {'Municipal': '1', 'Particular subvencionado': '2', 'Particular pagado': '3', 'Administración delegada': '4'}
+rural_dict = {'Urbano': '0', 'Rural': '1'}
+estado_estab_dict = {'Vigente': '1', 'Cerrado': '2', 'En receso': '3', 'Sin actividad': '4'}
+grado_dict = {
+    '1° Básico': '1',
+    '2° Básico': '2',
+    '3° Básico': '3',
+    '4° Básico': '4',
+    '5° Básico': '5',
+    '6° Básico': '6',
+    '7° Básico': '7',
+    '8° Básico': '8',
+    '1° Medio': '9',
+    '2° Medio': '10',
+    '3° Medio': '11',
+    '4° Medio': '12'
+}
+jor_dict = {'Mañana': '1', 'Tarde': '2', 'Vespertina': '3'}
+gen_dict = {'Masculino': '1', 'Femenino': '2'}
+
+# Entradas del usuario con opciones descriptivas
+COD_DEPE2 = depe_dict[st.selectbox("Dependencia", list(depe_dict.keys()))]
+RURAL_RBD = rural_dict[st.selectbox("Ruralidad", list(rural_dict.keys()))]
+ESTADO_ESTAB = estado_estab_dict[st.selectbox("Estado Establecimiento", list(estado_estab_dict.keys()))]
+COD_GRADO = grado_dict[st.selectbox("Grado", list(grado_dict.keys()))]
+COD_JOR = jor_dict[st.selectbox("Jornada", list(jor_dict.keys()))]
+GEN_ALU = gen_dict[st.selectbox("Género", list(gen_dict.keys()))]
 EDAD_ALU = st.number_input("Edad Alumno", min_value=5, max_value=25, value=15)
 PROM_GRAL = st.number_input("Promedio General", min_value=10.0, max_value=70.0, value=50.0)
 ASISTENCIA = st.number_input("Asistencia (%)", min_value=0.0, max_value=100.0, value=90.0)
